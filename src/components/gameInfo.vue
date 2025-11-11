9d00fc8c13402907890a49d049184a9f4d61017e
Oleksandr Borysenko <kontynium@upstars.com>
2025-11-11 16:22:09 +0100

    remove typo

--- src/components/gameInfo.vue
+++ src/components/gameInfo.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="game_info_container" :class="{ 'no-background': props.externalLink }">
-    //
     <!-- Стандартные элементы отображаются только если нет external_link -->
     <template v-if="!props.externalLink">
       <img class="game_info_thumb" :src="props.thumb" alt="" />
